a55a465da102ff3b2e4d9de38f0135e8397019b4
Kota Mizushima <mizukota@gmail.com>
2020-01-02 01:02:26 +0900

    Configure interval

--- src/main/scala/com/github/kmizu/pi4s/Main.scala
+++ src/main/scala/com/github/kmizu/pi4s/Main.scala
@@ -3,12 +3,12 @@ package com.github.kmizu.pi4s
 object Main {
   def main(args: Array[String]): Unit = {
     val times = if(args.length == 0) 10 else args(0).toInt
+    val interval = if(args.length < 2) 1000 else args(1).toInt
     val pin = PiGpio.output(GPIO_25)
     def blink(times: Int): Unit = {
       for(_ <- 0 until times) {
-        Thread.sleep(1000)
+        Thread.sleep(interval)
         pin.toggle()
-        Thread.sleep(1000)
       }
     }
     blink(times)
